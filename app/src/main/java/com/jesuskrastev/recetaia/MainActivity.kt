5798de4c3bf1780543a80c738f90eafd0146e7e6
JesusKrastev <jesuskrastev@gmail.com>
2025-08-05 12:46:28 +0200

    Initial commit

---
+++ app/src/main/java/com/jesuskrastev/recetaia/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jesuskrastev.recetaia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jesuskrastev.recetaia.ui.theme.RecetaIATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecetaIATheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
