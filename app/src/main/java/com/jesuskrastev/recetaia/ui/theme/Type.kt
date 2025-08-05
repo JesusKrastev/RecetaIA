39ab650249f719c29980a9513001f8c289f6d51b
JesusKrastev <jesuskrastev@gmail.com>
2025-08-05 21:25:39 +0200

    Refactor: Implement custom color scheme and typography
    This commit introduces a custom color scheme based on Material Design 3 principles and integrates the Poppins font family for typography.
    
    The color scheme includes definitions for light, dark, and medium/high contrast variations for both light and dark themes.
    
    The Poppins font family has been added to the project resources and is now used as the default font for the application's typography.

--- app/src/main/java/com/jesuskrastev/recetaia/ui/theme/Type.kt
+++ app/src/main/java/com/jesuskrastev/recetaia/ui/theme/Type.kt
@@ -1,34 +1,39 @@
 package com.jesuskrastev.recetaia.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.jesuskrastev.recetaia.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+@Composable
+fun PoppinsTypography() = Typography().run {
+    val fontFamily = FontFamily(
+        Font(R.font.poppins_extra_bold, FontWeight.ExtraBold),
+        Font(R.font.poppins_bold, FontWeight.Bold),
+        Font(R.font.poppins_semi_bold, FontWeight.SemiBold),
+        Font(R.font.poppins_medium, FontWeight.Medium),
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_light, FontWeight.Light),
+        Font(R.font.poppins_extra_light, FontWeight.ExtraLight)
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+    this.copy(
+        displayLarge = displayLarge.copy(fontFamily = fontFamily),
+        displayMedium = displayMedium.copy(fontFamily = fontFamily),
+        displaySmall = displaySmall.copy(fontFamily = fontFamily),
+        headlineLarge = headlineLarge.copy(fontFamily = fontFamily),
+        headlineMedium = headlineMedium.copy(fontFamily = fontFamily),
+        headlineSmall = headlineSmall.copy(fontFamily = fontFamily),
+        titleLarge = titleLarge.copy(fontFamily = fontFamily),
+        titleMedium = titleMedium.copy(fontFamily = fontFamily),
+        titleSmall = titleSmall.copy(fontFamily = fontFamily),
+        bodyLarge = bodyLarge.copy(fontFamily = fontFamily),
+        bodyMedium = bodyMedium.copy(fontFamily = fontFamily),
+        bodySmall = bodySmall.copy(fontFamily = fontFamily),
+        labelLarge = labelLarge.copy(fontFamily = fontFamily),
+        labelMedium = labelMedium.copy(fontFamily = fontFamily),
+        labelSmall = labelSmall.copy(fontFamily = fontFamily),
     )
-    */
-)
+}
